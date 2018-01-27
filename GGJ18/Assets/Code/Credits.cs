1a406ebfa304b3bfe27329e1c280a94a2a190f81
traguill <traguill1@gmail.com>
2018-01-27 04:23:57 +0100

    Menu + credits navigables

---
+++ GGJ18/Assets/Code/Credits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+	void Update () {
+	    if(Input.GetKeyUp(KeyCode.Space))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+	}
+}
